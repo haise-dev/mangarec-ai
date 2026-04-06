dcb78a5757ae00211b62093515767720858cd950
vietHoang21 <Hoang21112004@gmail.com>
2026-04-06 18:57:49 +0700

    chore: setup modular monolithic structure for AI, BE, and FE services p2

---
+++ be-service/src/main/java/com/mangarec/MangarecBeApplication.java
@@ -0,0 +1,13 @@
+package com.mangarec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangarecBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangarecBeApplication.class, args);
+	}
+
+}
---
+++ be-service/src/test/java/com/mangarec/MangarecBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangarec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangarecBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

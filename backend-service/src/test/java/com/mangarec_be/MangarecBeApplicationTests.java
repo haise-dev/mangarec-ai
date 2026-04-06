68a79fa7ac64715a746972d47b65038c5d008d53
vietHoang21 <Hoang21112004@gmail.com>
2026-04-06 18:46:20 +0700

    init commit

---
+++ backend-service/src/test/java/com/mangarec_be/MangarecBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangarec_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangarecBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
